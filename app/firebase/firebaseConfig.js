e5a2579e63280c52acfb30ba8c93f592fc1827fb
Ali Gohar <aligoharkp@gmail.com>
2024-08-20 09:44:42 +0500

    done

--- app/firebase/firebaseConfig.js
+++ app/firebase/firebaseConfig.js
@@ -4,16 +4,15 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDvqhl5_0Tf9uJKScbR8YJLP8JeN7B2UQI",
-  authDomain: "ai-flashcards-3cc8e.firebaseapp.com",
-  projectId: "ai-flashcards-3cc8e",
-  storageBucket: "ai-flashcards-3cc8e.appspot.com",
-  messagingSenderId: "435350297634",
-  appId: "1:435350297634:web:e94887e51b526dd7c13e7b"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
